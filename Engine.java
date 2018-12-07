e2be7293ec935a1e54cab83fa23baf89c2d295ad
GuillaumeLopez <45453888+GuillaumeLopez@users.noreply.github.com>
2018-12-07 09:23:29 +0100

    Update Engine.java

--- Engine.java
+++ Engine.java
@@ -22,12 +22,13 @@ public class Engine {
 		score= new int[] {0,0};
 		selection=new ArrayList<Bille>();
 	}
-		public ArrayList<Bille> getVoisin(bille) {
+	
+	public ArrayList<Bille> getVoisin(bille) {
 		int color = getColor(bille);
 		int i =getLocation(bille)[0];
 		int j =getLocation(bille)[1];
-		ArrayList<Bille> billes_meme_couleur = {};
-		ArrayList<Bille> voisins = {};
+		ArrayList<Bille> billes_meme_couleur = new ArrayList<Bille>;
+		ArrayList<Bille> voisins = new ArrayList<Bille>;
 		if (color == 1) {
 			billes_meme_couleur = BillesB
 		}
@@ -35,15 +36,22 @@ public class Engine {
 			billes_meme_couleur = BillesN
 		}
 		if 
-		ArrayList<ArrayList<int>> voisins_possibles = {{i+1,j}{i-1,j}{i,j-1}{i,j+1}};
+		ArrayList<ArrayList<int>> voisins_possibles = new ArrayList<ArrayList<int>>;
+		voisins_possibles.add({i+1,j});
+		voisins_possibles.add({i-1,j});
+		voisins_possibles.add({i,j-1});
+		voisins_possibles.add({i,j+1});
 		if (i<5) {
-			voisins_possibles.add({i-1,j-1},{i+1,j+1});
+			voisins_possibles.add({i-1,j-1});
+			voisins_possibles.add({i+1,j+1});
 		}
 		else if (i==5) {
-			voisins_possibles.add({i-1,j-1},{i-1,j+1})
+			voisins_possibles.add({i-1,j-1});
+			voisins_possibles.add({i-1,j+1});
 		}
 		else {
-			voisins_possibles.add({i+1,j-1},{i-1,j+1})
+			voisins_possibles.add({i+1,j-1});
+			voisins_possibles.add({i-1,j+1});
 		}
 		//test est dans le plateau ?
 		for (ArrayList<int> location : voisins_possibles) {
@@ -55,7 +63,25 @@ public class Engine {
 		}
 		return voisins
 	}
+	
+	public ArrayList<Bille> getAlignes(bille1, bille2) {
+		ArrayList<Bille> voisins_directs = new ArrayList<Bille>;
+		voisins_directs = getVoisins(bille1);
+		voisins_directs.add(getVoisins(bille2));
+		ArrayList<Bille> alignes = new ArrayList<Bille>;
+		for (Bille b : voisins_directs) {
+			if (b==bille1){
+				voisins_directs.remove(b);
+			}
+			else if (b==bille2){
+				voisins_directs.remove(b)
+			}
+		//for (Bille b : voisins_direct){
+			//if(testAlignement(b, bille1, bille2)==true){
+				//alignes.add(b)
+		return alignes
 		
+	}	
 
 	public Plateau getPlate() {
 		return plate;
