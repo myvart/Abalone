1039998bc5aaa1841e0c5be4ac19fff1efd803be
GuillaumeLopez <45453888+GuillaumeLopez@users.noreply.github.com>
2018-12-07 08:31:32 +0100

    Update Engine.java

--- Engine.java
+++ Engine.java
@@ -22,12 +22,39 @@ public class Engine {
 		score= new int[] {0,0};
 		selection=new ArrayList<Bille>();
 	}
-	public ArrayList<Bille> getVoisins(Bille b){
-		int i = b.location[0]
-		int j = b.location[1]
-		ArrayList<Bille> voisins = []
-		//remplissage de la liste
-		return (voisins)
+		public ArrayList<Bille> getVoisin(bille) {
+		int color = getColor(bille);
+		int i =getLocation(bille)[0];
+		int j =getLocation(bille)[1];
+		ArrayList<Bille> billes_meme_couleur = {};
+		ArrayList<Bille> voisins = {};
+		if (color == 1) {
+			billes_meme_couleur = BillesB
+		}
+		else {
+			billes_meme_couleur = BillesN
+		}
+		if 
+		ArrayList<ArrayList<int>> voisins_possibles = {{i+1,j}{i-1,j}{i,j-1}{i,j+1}};
+		if (i<5) {
+			voisins_possibles.add({i-1,j-1},{i+1,j+1});
+		}
+		else if (i==5) {
+			voisins_possibles.add({i-1,j-1},{i-1,j+1})
+		}
+		else {
+			voisins_possibles.add({i+1,j-1},{i-1,j+1})
+		}
+		//test est dans le plateau ?
+		for (ArrayList<int> location : voisins_possibles) {
+			for (Bille b : bille_meme_couleur) {
+				if ( location == getLocation(b)) {
+					voisins.add(b)
+				}
+			}
+		}
+		return voisins
+	}
 		
 
 	public Plateau getPlate() {
